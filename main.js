f93e18f037b33a5ee8f3bcfc644e271642627b2d
Johan <mellstroemjohan@gmail.com>
2021-05-10 11:57:27 +0200

    Möjliggjort för NPM paket

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("det här är main.js..2")
 
-function test(){
+//function test(){
+module.exports.test = function(){
     console.log("det här är en funktion som kan köras men inte gjort det")
 }
